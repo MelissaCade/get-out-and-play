e8f4ac178606844d3b0846b0a7759cf2c93a9762
BookWyrmMom <160056156+BookWyrmMom@users.noreply.github.com>
2024-03-12 19:20:24 -0600

    changed API site and references for national parks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,9 @@
-// curl -X 'GET' \
-//   'https://ridb.recreation.gov/api/v1/recareas?limit=50&offset=0&state=CO&lastupdated=10-01-2018' \
-//   -H 'accept: application/json' \
-//   -H 'apikey: 185ece11-651c-4751-9493-bea8717c3a9a'
-
-// https://ridb.recreation.gov/api/v1/recareas?limit=50&offset=0&state=CO&lastupdated=10-01-2018
-// the above is an example of the request url for colorado. We can change the state easily when we make the actual request.
-
-// we can use "RecAreaName" for the name of the Rec Area
-// we can use "RecAreaDescription" for the description
-// we can use "RecAreaLatitude" and "RecAreaLongitude" for coordinates
-// we should also pull the "RecAreaID" to plug into the media request for a picture
-
-// curl -X 'GET' \
-//   'https://ridb.recreation.gov/api/v1/recareas/708/media?query=image&limit=50&offset=0' \
-//   -H 'accept: application/json' \
-//   -H 'apikey: 185ece11-651c-4751-9493-bea8717c3a9a'
-
-// https://ridb.recreation.gov/api/v1/recareas/708/media?query=image&limit=50&offset=0
-// in this example, the "708" is the RecAreaID, so we will need to replace that as requested
-// some of the media is videos, but if we put the "query=image" bit in, as above, it filters them out
-// not all of the rec areas have photos, so we should plan a generic "national parks" filler photo
-
+//curl -X GET
+// "https://developer.nps.gov/api/v1/parks?stateCode=CO&api_key=CIOhNHwWVZAX8YbB1U7TJWA0Q8aazIZthMXdZLmY"
+// -H "accept: application/json"
+//here we see that the key is a part of the url. I used a stateCode of "CO" here to test, but that can be exchanged for any two-character state code
+//we can use "fullName" to get the name of the park
+//we can use "description" for the description
+//"latitude" and "longitude" can be used with the weather API to get local weather data
+//alternately, "addresses.postalCode" can be used to get a five-digit postal code
+//"images.url" will give a lovely .jpg of the park
